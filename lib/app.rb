929b3d7fc5fd89aabb16fbe47206502a7ce2103c
Sindre <sindre.nygard@capgemini.com>
2016-09-23 15:05:22 +0200

    me break

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,7 @@ require "sinatra/json"
 require_relative "radar"
 
 class FlightSchool < Sinatra::Base
-  get "/" do
+a  get "/" do
     erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX"] }
   end
 
